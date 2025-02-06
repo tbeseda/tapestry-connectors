44489a8217628002500a50d786040f011ab753ef
Taylor Beseda <tbeseda@gmail.com>
2025-02-05 17:13:32 -0700

    add Mystic9Ball tutorial

---
+++ Connectors/com.usetapestry.mystic9ball/plugin.js
@@ -0,0 +1,40 @@
+var lastUpdate = null;
+
+function load() {
+  if (lastUpdate != null) {
+    // check the interval provided by the user
+    console.log(`interval = ${interval}`);
+    let delta = parseInt(interval) * 60000; // minutes → milliseconds
+    let future = (lastUpdate.getTime() + delta);
+    let now = (new Date()).getTime();
+    if (now < future) {
+      // time has not elapsed, return no results
+      console.log(`time until next update = ${(future - now) / 1000} sec.`);
+      processResults(null);
+      return;
+    }
+  }
+
+  const endpoint = `${site}/api`;
+  sendRequest(endpoint)
+    .then((text) => {
+      const json = JSON.parse(text);
+
+      let uri = site + `?value=${json.value}&timestamp=${json.timestamp}`;
+      let date = new Date(json.timestamp * 1000); // seconds → milliseconds
+
+      let src = "https://usetapestry.com" + json.image; // relative → absolute url
+
+      let item = Item.createWithUriDate(uri, date);
+      item.body = `<p>The Mystic 9-Ball says: ${json.description}<img src="${src}" /></p>`;
+
+      let items = [item];
+
+      processResults(items);
+
+      lastUpdate = new Date();
+    })
+    .catch((requestError) => {
+      processError(requestError);
+    });
+}
